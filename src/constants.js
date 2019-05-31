7fd73a97e64f1d44f2684fc493311a9d52c28212
Fabien Cazenave <fabien@cazenave.cc>
2019-05-31 14:57:02 +0200

    a thinner key padding looks better

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 export const KEY_WIDTH = 60;  // 1U = 0.75" = 19.05mm = 60px
-export const KEY_PADDING = 5; // 10px between two key edges
+export const KEY_PADDING = 4; // 8px between two key edges
 export const KEY_RADIUS = 5;  // 5px border radius
